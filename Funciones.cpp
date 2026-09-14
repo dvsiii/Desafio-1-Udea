bcea06ac8b3cedd8b95b54a06344683edd4a2181
dvsi <dclsmcr233@gmail.com>
2026-09-13 19:53:22 -0500

    Inicio de introducción, variables e inicio de funciones

---
+++ Funciones.cpp
@@ -0,0 +1,32 @@
+int relleno()
+{
+    return 0;
+}
+int lectura()
+{
+    return 0;
+}
+int mover()
+{
+    return 0;
+}
+int ag_fila()
+{
+    return 0;
+}
+int ag_columna()
+{
+    return 0;
+}
+int del_fila()
+{
+    return 0;
+}
+int del_columna()
+{
+    return 0;
+}
+int del_elemento()
+{
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
-
+#include "Llamado.h"
 int main()
 {
+    cout << "Bienvenido a Sweet Crush" << endl;
+    //Introduccion
+    short unsigned int filas=4;
+    short unsigned int columna=6;
+    unsigned int puntuacion=0;
+    unsigned int cascadas=0;
+    bool salir=false;
+    unsigned char **ptablero=NULL;
+    //Variables
+    while(salir==true){
+
+    }
     return 0;
 }
