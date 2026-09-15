0ace1ff99b6850448468633e628257686eb3b29b
dvsi <dclsmcr233@gmail.com>
2026-09-15 18:46:46 -0500

    Se añadio un arreglo con las fichas y se empezo el trabajo de funciones empezando la funcion relleno

--- Funciones.cpp
+++ Funciones.cpp
@@ -1,12 +1,22 @@
-int relleno()
+int relleno(int fila, int columnas, int columna, unsigned char *puntero)
+{
+    int k=0;
+    for(int i=0; i<columna+1;++i){
+        k++;
+    }
+    int bit_relleno=(fila*columnas+columna)*3;
+    int desplazo=bit_relleno%8;
+    int bit_inicio=bit_relleno/8;
+    int valor=(fila+columna*k)%7;
+    valor=valor<<desplazo;
+    puntero[bit_inicio]=puntero[bit_inicio]|valor;
+    return 0;
+}
+int lectura(unsigned char *puntero)
 {
     return 0;
 }
-int lectura()
-{
-    return 0;
-}
-int mover()
+int mover(unsigned char *puntero)
 {
     return 0;
 }
--- main.cpp
+++ main.cpp
@@ -10,12 +10,18 @@ int main()
     unsigned int puntuacion=0;
     unsigned int cascadas=0;
     bool salir=false;
+    char fichas[8]="ABCDEFQ";
     unsigned char *ptablero=NULL;
     unsigned char *pcopia=NULL;
     int bits=columna*3*filas;
     int bytes=(bits+7)/8;
     //Variables
     ptablero= new unsigned char[bytes];
+    for(int i=0;i<filas;++i){
+        for(int j=0;j<columna;++j){
+            //Aqui va la funcion relleno
+        }
+    }
     while(salir==false){
         cout<<filas<<"x"<<columna;
         cout<<puntuacion;
