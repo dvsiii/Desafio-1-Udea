78de04185fe6818e6762d193f076c2053b386110
dvsi <dclsmcr233@gmail.com>
2026-09-17 15:18:10 -0500

    Divicion de la funcion lectura en una para leer la ficha en especifico y leer el tablero, lectura_f ya esta lista, lectura_t en proceso

--- Funciones.cpp
+++ Funciones.cpp
@@ -17,16 +17,38 @@ int relleno(int fila, int columnas, int columna, unsigned char *puntero)
     }
     return 0;
 }
-int lectura(int fila, int columnas, int filas, int columna, unsigned char *puntero)
+int lectura_f(int fila, int columnas, int columna, unsigned char *puntero)
 {
     int bit_relleno=(fila*columnas+columna)*3;
     int bit_inicio=bit_relleno/8;
     int desplazo=bit_relleno%8;
-    int f_leida=puntero[bit_inicio]>>5;
+    int f_leida=0;
+    int desborde=puntero[bit_inicio+1]<<8;
+    desborde=desborde|puntero[bit_inicio];
+    f_leida=desborde>>desplazo;
     f_leida=f_leida&7;
+    return f_leida;
+}
+int lectura_t(int columnas, int filas, unsigned char *puntero)
+{
+    int f_leida=0;
+    for(int i=0;i<filas;i++){
+        for(int j=0;j<columnas;j++){
+            f_leida=lectura_f(i,columnas,j,puntero);
+            if(f_leida==0){
+                mover(i,j,puntero);
+            }
+            if(f_leida==lectura_f(i+1,columnas,j,puntero)&&f_leida==lectura_f(i+2,columnas,j,puntero)&&f_leida==0){
+                //vacio por ahora hasta que se haga del_elemento()
+            }
+            if(f_leida==lectura_f(i,columnas,j+1,puntero)&&f_leida==lectura_f(i,columnas,j+2,puntero)&&f_leida==0){
+                //Vacio por ahora
+            }
+        }
+    }
     return 0;
 }
-int mover(unsigned char *puntero)
+int mover(int fila, int columna, unsigned char *puntero)
 {
     return 0;
 }
--- Llamado.h
+++ Llamado.h
@@ -1,4 +1,6 @@
 #ifndef LLAMADO_H
 #define LLAMADO_H
 int relleno(int fila, int columnas, int columna, unsigned char *puntero);
+int lectura(int columnas, int filas, unsigned char *puntero);
+int mover(int fila, int columna, unsigned char *puntero);
 #endif // LLAMADO_H
